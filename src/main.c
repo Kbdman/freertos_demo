bec9adeb14efd759f1ca48227a0b965f6ff26634
kbdman <lihai1234@live.cn>
2024-08-20 01:23:48 +0800

    modify ld script, but unusable

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include "FreeRTOS.h"
 #include "task.h"
+extern void *DataVM, *DataStart;
+extern size_t DataSize;
+extern void *BSSVM, *BSSStart;
+extern size_t BSSSize;
+extern void *memcpy(void *dest, void *src, size_t n);
+extern void *memset(void *s, int c, size_t n);
 void main()
 {
+    memcpy(DataVM, DataStart, DataSize);
+    memcpy(BSSVM, BSSStart, BSSSize);
     vTaskStartScheduler();
     for (;;)
     {
