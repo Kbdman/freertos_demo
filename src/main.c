514be2262db48d29d6cc6e7321eb1436f91d6b83
kbdman <lihai1234@live.cn>
2024-08-20 23:39:34 +0800

    update ld script initialize data and bss setup some interrupt handlers

--- src/main.c
+++ src/main.c
@@ -1,19 +1,35 @@
 #include "FreeRTOS.h"
 #include "task.h"
-extern void *DataVM, *DataStart;
-extern size_t DataSize;
-extern void *BSSVM, *BSSStart;
-extern size_t BSSSize;
+#define ISR(x) ((void *)(&x))
+extern char DataVM, DataVMEnd, DataStart;
+
+extern char BSSVM, BSSVMEnd;
 extern void *memcpy(void *dest, void *src, size_t n);
 extern void *memset(void *s, int c, size_t n);
+extern void xPortPendSVHandler(void) __attribute__((naked));
+extern void xPortSysTickHandler(void);
+extern void vPortSVCHandler(void) __attribute__((naked));
 void main()
 {
-    memcpy(DataVM, DataStart, DataSize);
-    memcpy(BSSVM, BSSStart, BSSSize);
+    memcpy(&DataVM, &DataStart, &DataVMEnd - &DataVM);
+    memset(&BSSVM, 0, &BSSVMEnd - &BSSVM);
     vTaskStartScheduler();
     for (;;)
     {
     }
 }
-
-void *reset_vector[2] __attribute__((section(".isr"))) = {(void *)0x2001FFFF, (void *)(&main)};
+void DefaultHandler(void) {};
+void *reset_vector[] __attribute__((section(".isr"))) = {
+    (void *)0x2001FFFF,
+    (void *)(&main),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(vPortSVCHandler),
+    ISR(DefaultHandler),
+    ISR(DefaultHandler),
+    ISR(xPortPendSVHandler),
+    ISR(xPortSysTickHandler)};
